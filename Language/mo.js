bddb995cc9e87f7d186d47ea96dd69f46674d3e4
ochgerel <out5ide_ca5tle@yahoo.com>
2013-06-04 09:45:31 +0800

    Update and rename Language/locale/en.js to Language/mo.js
    mongolian language

---
+++ Language/mo.js
@@ -0,0 +1,36 @@
+/* English LANGUAGE 
+================================================== */
+if(typeof VMM != 'undefined') {
+	VMM.Language = {
+		lang: "en",
+		api: {
+			wikipedia: "en"
+		},
+		date: {
+			month: ["Нэгдүгээр сар", "Хоёрдугаар сар", "Гуравдугаар сар", "Дөрөвдүгээр сар", "Тавдугаар сар", "Зургадугаар", "Долдугаар сар", "Наймдугаар сар", "Есдүгээр сар", "Аравдугаар сар", "Арваннэгдүгээр сар", "Арванхоёрдугаар сар"],
+			month_abbr: ["1-сар.", "2-сар.", "3-сар.", "4-сар.", "5-сар.", "6-сар.", "7-сар.", "8-сар.", "9-сар.", "10-сар.", "11-сар.", "12-сар."],
+			day: ["Ням","Даваа", "Мягмар", "Лхагва", "Пүрэв", "Баасан", "Бямба"],
+			day_abbr: ["7.","1.", "2.", "3.", "4.", "5.", "6."]
+		}, 
+		dateformats: {
+			year: "yyyy",
+			month_short: "mmm",`
+			month: "mmmm yyyy",
+			full_short: "mmm d",
+			full: "mmmm d',' yyyy",
+			time_no_seconds_short: "h:MM TT",
+			time_no_seconds_small_date: "h:MM TT'<br/><small>'mmmm d',' yyyy'</small>'",
+			full_long: "mmm d',' yyyy 'at' h:MM TT",
+			full_long_small_date: "h:MM TT'<br/><small>mmm d',' yyyy'</small>'"
+		},
+		messages: {
+			loading_timeline: "Өгөгдлийг уншиж байна... ",
+			return_to_title: "Return to Title",
+			expand_timeline: "Expand Timeline",
+			contract_timeline: "Contract Timeline",
+			wikipedia: "From Wikipedia, the free encyclopedia",
+			loading_content: "Loading Content",
+			loading: "Уншиж байна"
+		}
+	}
+}
